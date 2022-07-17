01c2c6ae1c7e3451613858deb4a58cce151ded2c
kejlor <woj450@gmail.com>
2022-07-17 15:25:41 +0200

    Initial Commit

---
+++ CarMarketApp/CarMarketAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarMarketAppApp.swift
+//  CarMarketApp
+//
+//  Created by Bartosz Wojtkowiak on 17/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct CarMarketAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
