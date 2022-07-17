f2e0df736edac9363ee03b9fb61db5efe74344d0
kejlor <woj450@gmail.com>
2022-07-17 18:16:10 +0200

    Added filtered car list

--- CarMarketApp/ViewModels/CarListViewModel.swift
+++ CarMarketApp/ViewModels/CarListViewModel.swift
@@ -9,6 +9,7 @@ import Foundation
 
 class CarListViewModel: ObservableObject {
     @Published var cars: [CarViewModel] = []
+    @Published var filteredCars: [CarViewModel] = []
     
     func getCars() {
         guard let url = URL(string: "https://tinuon.net/api/cars") else { return }
@@ -20,7 +21,18 @@ class CarListViewModel: ObservableObject {
             case .success(let cars):
                 DispatchQueue.main.async {
                     self.cars = cars.map(CarViewModel.init)
+                    self.filterCars()
                 }
+            }
+        }
+    }
+    
+    func filterCars() {
+        filteredCars = []
+        
+        cars.forEach { car in
+            if car.availability {
+                filteredCars.append(car)
             }
         }
     }
