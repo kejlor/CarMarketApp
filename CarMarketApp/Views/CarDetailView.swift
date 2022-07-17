63a82702e40710d57b96513c4dc2cd010cdca7c6
kejlor <woj450@gmail.com>
2022-07-17 18:59:19 +0200

    Inicial commit

---
+++ CarMarketApp/Views/CarDetailView.swift
@@ -0,0 +1,20 @@
+//
+//  CarDetailView.swift
+//  CarMarketApp
+//
+//  Created by Bartosz Wojtkowiak on 17/07/2022.
+//
+
+import SwiftUI
+
+struct CarDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CarDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        CarDetailView()
+    }
+}
